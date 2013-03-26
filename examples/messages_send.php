22ece71734e53320e17b137e07547fb421139ab5
chadwick37 <chad@minuszeromedia.com>
2013-03-26 13:31:06 -0600

    changed request to array from json

--- examples/messages_send.php
+++ examples/messages_send.php
@@ -7,8 +7,24 @@
 require('../Mandrill.php');
 require('config.php');
 
-$request_json = '{"type":"messages","call":"send","message":{"html": "<h1>example html</h1>", "text": "example text", "subject": "example subject", "from_email": "wes@werxltd.com", "from_name": "example from_name", "to":[{"email": "wes@reasontostand.org", "name": "Wes Widner"}],"headers":{"...": "..."},"track_opens":true,"track_clicks":true,"auto_text":true,"url_strip_qs":true,"tags":["test","example","sample"],"google_analytics_domains":["werxltd.com"],"google_analytics_campaign":["..."],"metadata":["..."]}}';
-
-$ret = Mandrill::call((array) json_decode($request_json));
-
-print_r($ret);
+// since we are using php I prefer to keep my data in an array
+$request = array (
+  				"type" => "messages",
+					"call" => "send",
+					"message" => array (
+						"html" => "<h1>example html</h1>", 
+						"text" => "example text", 
+						"subject" => "example subject", 
+						"from_email" => "chad@minuszeromedia.com", 
+						"from_name" => "Chad", 
+						"to" => array(
+								array(
+								"email" => "chadwick37@gmail.com", 
+								"name" => "Chad Baron"
+								)
+							),
+						)
+					);
+				
+				$ret = Mandrill::call($request);
+				echo json_encode($ret);
